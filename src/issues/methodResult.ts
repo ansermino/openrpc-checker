849c3d8997d240342156a43d04f093a6616534cd
David Ansermino <14164624+ansermino@users.noreply.github.com>
2024-12-27 21:49:59 -0500

    Improve messages and object printing

--- src/issues/methodResult.ts
+++ src/issues/methodResult.ts
@@ -1,17 +1,19 @@
 import {ContentDescriptorObjectRequired, JSONSchema} from '@open-rpc/meta-schema';
 import {DiffIssue} from './index';
+import {diff} from 'deep-object-diff';
 
 export class MethodResultIssue_Required extends DiffIssue {
   constructor(
     expected: ContentDescriptorObjectRequired | undefined,
     actual: ContentDescriptorObjectRequired | undefined,
   ) {
-    super(`Method result ${expected ? 'should' : "shouldn't"} be required. Actual: ${actual}`);
+    super(`Method result ${expected ? 'should' : "shouldn't"} be required (actual: ${actual})`);
   }
 }
 
 export class MethodResultIssue_Schema extends DiffIssue {
   constructor(expected: JSONSchema, actual: JSONSchema) {
-    super(`Expected:\n ${JSON.stringify(expected)}\nActual: ${JSON.stringify(actual)}`);
+    const d = diff(expected as object, actual as object);
+    super(`Incorrect result schema:\nDiff:\n  ${JSON.stringify(d, null, 2)}`);
   }
 }
