3af1e60a263e46a5d83fb6966d967c8df56a0c78
David Ansermino <14164624+ansermino@users.noreply.github.com>
2025-01-06 18:42:03 -0500

    Adds ref parser cmds

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import {program} from 'commander';
-import {parseOpenRPCDocument} from '@open-rpc/schema-utils-js';
-import {diff} from './diff';
-import {printDiff} from './output';
+import {checkCmd, diffCmd} from './cmd';
 
 program.name('openrpc').version('0.0.0').description('');
 
@@ -10,15 +8,39 @@ program
   .description('Compare to OpenRPC documents')
   .option('-s, --spec <PATH>', 'Path to spec file')
   .option('-t, --target <PATH>', 'Path to target file')
+  .option('--ref-parser', 'Use json-schema-ref-parser instead of schema-utils-js', false)
   .action(async (options) => {
-    const spec = await parseOpenRPCDocument(options.spec);
-    const target = await parseOpenRPCDocument(options.target);
-
-    console.log(`Loaded spec with ${spec.methods.length} methods`);
-    console.log(`Loaded target with ${target.methods.length} methods`);
-
-    const res = diff(spec, target);
-    printDiff(res);
+    try {
+      await diffCmd(options.spec, options.target, options.refParser);
+    } catch (e) {
+      program.error(e);
+    }
   });
+
+program
+  .command('check')
+  .description('Validate an OpenRPC document')
+  .option('-t, --target <PATH>', 'Path to target file')
+  .option('--ref-parser', 'Use json-schema-ref-parser instead of schema-utils-js', false)
+  .action(async (options) => {
+    try {
+      await checkCmd(options.target, options.refParser);
+    } catch (e) {
+      program.error(e.message.toString());
+    }
+  });
+
+function errorColor(str: string) {
+  // Add ANSI escape codes to display text in red.
+  return `\x1b[31m${str}\x1b[0m`;
+}
+
+program.configureOutput({
+  // Visibly override write routines as example!
+  writeOut: (str) => process.stdout.write(`${str}`),
+  writeErr: (str) => process.stdout.write(`${str}`),
+  // Highlight errors in color.
+  outputError: (str, write) => write(errorColor(str)),
+});
 
 program.parse(process.argv);
